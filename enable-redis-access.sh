ca0740f907b93dcb72c85eef30b221c03db410fa
anandmohnish <manand1@hawk.iit.edu>
2018-04-26 20:57:35 -0500

    commented distclean

--- enable-redis-access.sh
+++ enable-redis-access.sh
@@ -12,6 +12,6 @@ sudo yum install gcc
 wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
-make distclean  // Ubuntu systems only
+#make distclean  // Ubuntu systems only
 make
 src/redis-cli -c -h mycachecluster.eaogs8.0001.usw2.cache.amazonaws.com -p 6379
