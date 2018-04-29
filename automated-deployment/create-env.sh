91cfda27532c9c7d8dcac9f91188c1489de8c8fc
anandmohnish <manand1@hawk.iit.edu>
2018-04-29 10:06:51 -0500

    disabled public IP assignmnet for ec2

--- automated-deployment/create-env.sh
+++ automated-deployment/create-env.sh
@@ -69,8 +69,7 @@ echo $subnets
 
 #Creating Launch Config
 aws autoscaling create-launch-configuration --key-name $key_pair --launch-configuration-name redis-lc \
---image-id $ami_id --instance-type t2.micro --security-groups $sg --instance-monitoring Enabled=true --user-data file://enable-redis-access.sh
-#--no-associate-public-ip-address
+--image-id $ami_id --instance-type t2.micro --security-groups $sg --instance-monitoring Enabled=true --user-data file://enable-redis-access.sh --no-associate-public-ip-address
 
 #Creating Load Balancer and Target Group
 LB_ARN=$(aws elbv2 create-load-balancer --name redis-lb \
